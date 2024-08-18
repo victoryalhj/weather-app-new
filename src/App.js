166af6371b44d42659f0aeb87157812ca83c11ec
victoryalhj <victoryalhj@gmail.com>
2024-08-18 16:04:26 +0100

    data updated

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import './App.css';
 function App() {
 
   const getCurrentLocation=()=>{
-    console.log("getCurrentLocation");
+    navigator.geolocation.getCurrentPosition((position)=>{
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat,lon)
+    });
   };
+
+  const getWeatherByCurrentLocation = async(lat,lon) => {
+    let url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=faa38f17e7999e9f8f3778867a829169`
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data",data);
+  };
+
   useEffect(()=>{
     getCurrentLocation();
-  },[])
+  },[]);
 
   return (
 
